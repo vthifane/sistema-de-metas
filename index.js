340b9344b6e9bc351202938160e66558176b127d
Vitória Thifane <vthifanepx@gmail.com>
2024-10-01 19:39:45 -0300

    Atualizações de metas em aberto

--- index.js
+++ index.js
@@ -22,12 +22,14 @@ const listarMetas = async () => {
     //respostas é a seleção de metas
     const respostas = await checkbox({
         message: 'Use as setas para mudar de meta, espaço para marcar ou desmarcar e o enter para finalizar a etapa',
-        //spread
+        //spread/rest operator: ...
         choices: [...metas],
         //instructions esta false para mostar apenas as intruçoes em portugues no message
         instructions: false
     })
-    //essa função vai marcar todas as metas como false (desmarcar todas as metas)
+    //essa função vai marcar todas as metas como false(desmarcar todas as metas)
+    //colocar essa função desmarcar antes do if para que não ocorra o erro de 
+    //não conseguir desmarcar
     metas.forEach((m)=> {
         m.checked = false
     })
@@ -36,7 +38,6 @@ const listarMetas = async () => {
         console.log("Nenhuma meta selecionada")
         return
     }
-   
     //esta função vai usar o metodo 'paraCada' vai comparar cada resposta da const respostas
     // usando o metodo 'find' da função meta para retornar o value da resposta compatível
     // e marcar como true 
@@ -59,9 +60,29 @@ const metasRealizadas = async () => {
     }
     await select({
         message: 'Metas realizadas',
+        //neste caso o spread operator está pegando o array "realizadas" gerado após a execução
+        //do metodo "filter"e colocando dentro do array que esta no choices
         choices: [...realizadas]
     })
 }
+//esta é uma função assincrona que recebe a constante "abertas" como argumento que é um array
+//que esta recebendo o metodo "filter" que é uma higer order function(metodo que recebe uma funçao)
+//para retornar as metas com checked != de true
+const metasAbertas = async () => {
+    const  abertas = metas.filter((meta) => {
+        //return !metas.checked
+        return meta.chacked != true
+    }) 
+    if(abertas.length == 0){
+        console.log('Não existem metas abertas :)')
+        return
+    }
+    await select({
+        message: 'Metas Abertas',
+        choices: [...abertas]
+    })
+}
+
 
 const start = async () => {
     while(true) {
@@ -82,6 +103,10 @@ const start = async () => {
                 value: 'realizadas'
             },
             {
+                name: 'Metas Abertas',
+                value: 'abertas'
+            },
+            {
                 name: 'Sair',
                 value: 'sair'
             }
@@ -98,12 +123,13 @@ const start = async () => {
             case 'realizadas':
                 await metasRealizadas()
                 break
+            case 'abertas':
+                await metasAbertas()
+                break
             case 'sair':
                 return
         }
     }
 }
-
- 
 
 start()
--- toDo.md
+++ toDo.md
@@ -5,6 +5,6 @@
 * remover metas
 * sistema de mensagem 
 
-const { select } = require('@inquirer/prompts')
+
 
 * higher order functions HOF: são metodos que recebem  uma função
